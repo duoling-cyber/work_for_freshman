f04f39420c6771912521d2005b8f6e8e133f5845
duoling-cyber <126667401+duoling-cyber@users.noreply.github.com>
2023-05-07 15:06:43 +0800

    Bootstraplayout

---
+++ helloworld.js
@@ -0,0 +1,18 @@
+document.write('hello,world!');
+function sayhello(){
+  var response = prompt('waht is your name');
+  alert('hello'+response+',have a nice day!');
+
+}
+
+function add(a,b){
+  return a + b;
+}
+console.log(add(20,30));
+console.log(add(18,17));
+
+function showTopic() {
+  var x = document.getElementById('demo');
+  x.style.fontSize ='25px';
+  x.style.color ='red';
+}
